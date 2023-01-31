084cf4989e2fceaa20f33b21aac8dfc8ae079530
Megan Shaw <mmeganshaw@gmail.com>
2023-01-31 18:35:08 -0500

    Site structure and nav/footer

--- server.js
+++ server.js
@@ -16,6 +16,41 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/careers', (req, res) => {
+    res.render('careers');
+});
+
+app.get('/gallery', (req, res) => {
+    res.render('gallery');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/services', (req, res) => {
+    res.render('service-landing');
+});
+
+app.get('/services/lawn-maintenance', (req, res) => {
+    res.render('lawn-maintenance');
+});
+
+app.get('/services/landscape-install', (req, res) => {
+    res.render('landscape-installation');
+});
+
+app.get('/services/snow-removal', (req, res) => {
+    res.render('snow-ice');
+});
+
+app.get('/services/landscape-design', (req, res) => {
+    res.render('landscape-design');
+});
 
 const PORT = process.env.PORT || 3000;
 
